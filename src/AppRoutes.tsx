5e9cbb1bd6a9b9045318084e1004211ce34056d1
guilhermedsc <guilhermescdev@gmail.com>
2025-06-30 08:54:52 -0300

    create app routes to manage app routes

---
+++ src/AppRoutes.tsx
@@ -0,0 +1,20 @@
+// src/AppRoutes.tsx
+import { Routes, Route } from "react-router"
+import App from './App'
+import AgentLoginPage from "./components/auth/AgentLoginPage"
+import AgentRegisterPage from "./components/auth/AgentRegisterPage"
+import ItemListPage from "./components/items/ItemListPage"
+
+export default function AppRoutes() {
+  return (
+    <Routes>
+      <Route path='/' element={<App />} />
+      <Route path='auth'>
+        <Route path='login' element={<AgentLoginPage />} />
+        <Route path='register' element={<AgentRegisterPage />} />
+      </Route>
+      <Route path='agent/item/list' element={<ItemListPage />} />
+      <Route path='public/item/list' element={<>teste</>} />
+    </Routes>
+  )
+}
