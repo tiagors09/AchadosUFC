67b99e8497612eaf63f89fdfa5f1103b28205701
guilhermedsc <guilhermescdev@gmail.com>
2025-06-30 08:55:02 -0300

    using app routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,12 @@
-import {
-  BrowserRouter,
-  Route,
-  Routes,
-} from "react-router"
-
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import AgentLoginPage from "./components/auth/AgentLoginPage/index.tsx"
-import AgentRegisterPage from "./components/auth/AgentRegisterPage/index.tsx"
-import { AuthProvider } from "./components/auth/AuthContext.tsx"
 import { Toaster } from "sonner"
-import { ItemProvider } from "./components/items/ItemContext.tsx"
-import ItemListPage from "./components/items/ItemListPage/index.tsx"
+import AppRoutes from './AppRoutes'
+import './index.css'
+
+import { AuthProvider } from "./components/auth/AuthContext"
+import { ItemProvider } from "./components/items/ItemContext"
+import { BrowserRouter } from 'react-router'
 
 const root = document.getElementById('root')!
 
@@ -23,14 +16,7 @@ createRoot(root).render(
       <ItemProvider>
         <BrowserRouter>
           <Toaster />
-          <Routes>
-            <Route path='/' element={<App />} />
-            <Route path='auth'>
-              <Route path='login' element={<AgentLoginPage />} />
-              <Route path='register' element={<AgentRegisterPage />} />
-            </Route>
-            <Route path='agent/item/list' element={<ItemListPage />} />
-          </Routes>  
+          <AppRoutes />
         </BrowserRouter>
       </ItemProvider>
     </AuthProvider>
