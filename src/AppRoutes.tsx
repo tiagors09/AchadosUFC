12bf63bc2dc535983c22cc9f5163f1c2ad747a68
Francisco Breno <fbreno.dev@gmail.com>
2025-07-13 16:22:45 -0300

    feat: Global Header.

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -5,29 +5,33 @@ import ItemListPage from "./components/items/ItemListPage";
 import { useAuth } from './components/auth/AuthContext';
 import { RetrievedItemProvider } from "./components/items/RetrievedItemContext";
 import { useItems } from './components/items/ItemContext';
+import { Header } from './components/layout/Header'; // Import the Header component
 
 export default function AppRoutes() {
   const { isAuthenticated } = useAuth();
   const { deleteItemImage } = useItems();
 
   return (
-    <Routes>
-      <Route path='/' element={
-        <RetrievedItemProvider isAuthenticatedContext={false} deleteItemImage={deleteItemImage}>
-          <ItemListPage editable={false} showRetrievedItemsSection={false} />
-        </RetrievedItemProvider>
-      } />
-      <Route path='auth'>
-        <Route path='login' element={<AgentLoginPage />} />
-        <Route path='register' element={<AgentRegisterPage />} />
-      </Route>
-      <Route path='agent/item/list' element={
-        isAuthenticated ?
-          <RetrievedItemProvider isAuthenticatedContext={true} deleteItemImage={deleteItemImage}>
-            <ItemListPage editable={true} showRetrievedItemsSection={true} />
-          </RetrievedItemProvider> :
-          <Navigate to='/auth/login'/>
-      } />
-    </Routes>
+    <>
+      <Header />
+      <Routes>
+        <Route path='/' element={
+          <RetrievedItemProvider isAuthenticatedContext={false} deleteItemImage={deleteItemImage}>
+            <ItemListPage editable={false} showRetrievedItemsSection={false} />
+          </RetrievedItemProvider>
+        } />
+        <Route path='auth'>
+          <Route path='login' element={<AgentLoginPage />} />
+          <Route path='register' element={<AgentRegisterPage />} />
+        </Route>
+        <Route path='agent/item/list' element={
+          isAuthenticated ?
+            <RetrievedItemProvider isAuthenticatedContext={true} deleteItemImage={deleteItemImage}>
+              <ItemListPage editable={true} showRetrievedItemsSection={true} />
+            </RetrievedItemProvider> :
+            <Navigate to='/auth/login'/>
+        } />
+      </Routes>
+    </>
   )
 }
